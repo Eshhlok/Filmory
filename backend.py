bdd0d990aa738e5d21f3ac942c344591d3d671fa
Eshlok Agarwal <eshlokagarwal@gmail.com>
2026-02-19 19:40:05 +0530

    nothing

--- backend.py
+++ backend.py
@@ -1,10 +1,5 @@
-from config import GENRE_MAP
 from data_store import load_movies
 from text_similarity import build_text_similarity
-
 movies_df = load_movies()
-movies_df.reset_index(drop=True, inplace=True)
-print(movies_df.iloc[0]["genre_ids"])
-print(GENRE_MAP)
-    
+movies_df.reset_index(drop=True, inplace=True)  
 tfidf, cosine_sim = build_text_similarity(movies_df)
